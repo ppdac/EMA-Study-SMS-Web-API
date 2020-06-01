312d4cd4e95326442378787b703928ed532b4be9
Louis Waweru <waweru@gmail.com>
2020-06-01 00:07:46 -0400

    ADDED: AB#8 Twilio credentials object (models folder) AB# Skeleton controller twilio library from nuget

---
+++ EMAStudySMSWebAPI/Controllers/TwilioIntermediaryController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+
+namespace EMAStudySMSWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwilioIntermediaryController : ControllerBase
+    {
+        // GET: api/TwilioIntermediary
+        //Should return some basic stats; are there queued items, failed sends, etc....
+        [HttpGet]
+        public string Get()
+        {
+            string status = "Not implemented.";
+            return status;
+        }
+
+        // POST: api/TwilioIntermediary/SMS_Object
+        //This should get a specific type of object as described in 
+        [HttpPost]
+        public string Post()
+        {
+            string status = "Not implemented.";
+            return status;
+
+        }
+    }
+}
---
+++ EMAStudySMSWebAPI/Models/Configuration/TwilioCredentials.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace EMAStudySMSWebAPI.Models.Configuration
+{
+    public class TwilioCredentials
+    {
+        public string AccountSID { get; set; }
+        public string AuthToken { get; set; }
+    }
+}

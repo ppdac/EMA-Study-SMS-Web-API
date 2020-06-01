7d68e513eb9a8d15f433531142bf660b58d22a68
Louis Waweru <waweru@gmail.com>
2020-06-01 01:39:20 -0400

    * TwilioIntermediaryController.cs: Resolves AB#2
    * EMAStudySMSWebAPI.csproj: Resolves AB#10
    
    * launchSettings.json: Stop launching a browser... work with Postman
      for now.
    
    * Startup.cs: added TwilioCredentials model
    
    * EMAStudySMSWebAPI.sln:

--- EMAStudySMSWebAPI/Controllers/TwilioIntermediaryController.cs
+++ EMAStudySMSWebAPI/Controllers/TwilioIntermediaryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using EMAStudySMSWebAPI.Models.Configuration;
+using EMAStudySMSWebAPI.Models.Scheduler;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -18,19 +20,35 @@ namespace EMAStudySMSWebAPI.Controllers
     [ApiController]
     public class TwilioIntermediaryController : ControllerBase
     {
-        // GET: api/TwilioIntermediary
-        //Should return some basic stats; are there queued items, failed sends, etc....
-        [HttpGet]
-        public string Get()
+        private readonly TwilioCredentials _twilioCredentials;
+
+        public TwilioIntermediaryController(IOptions<TwilioCredentials> twilioCredentials)
         {
-            string status = "Not implemented.";
-            return status;
+            if (twilioCredentials != null)
+                _twilioCredentials = twilioCredentials.Value;
+            else
+                throw new ArgumentException(nameof(twilioCredentials));
         }
 
         // POST: api/TwilioIntermediary/SMS_Object
         //This should get a specific type of object as described in 
         [HttpPost]
-        public string Post()
+        public HttpStatusCode Post(SMS sms)
+        {
+            TwilioClient.Init(_twilioCredentials.AccountSID, _twilioCredentials.AuthToken);
+            var options = new CreateMessageOptions(new Twilio.Types.PhoneNumber(sms.recipientPhoneNumber));
+            options.From = sms.senderPhoneNumber;
+            options.Body = sms.body;
+            var message = MessageResource.Create(options);
+            var statusCode = TwilioClient.GetRestClient().HttpClient.LastResponse.StatusCode;
+
+            return statusCode;
+        }
+
+        // GET: api/TwilioIntermediary
+        //Should return some basic stats; are there queued items, failed sends, etc....
+        [HttpGet]
+        public string Get()
         {
             string status = "Not implemented.";
             return status;

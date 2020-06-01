720a08a5d1091580f1014576497a9066cdaf7422
Louis Waweru <waweru@gmail.com>
2020-06-01 01:01:44 -0400

    * SMS.cs: * EMAStudySMSWebAPI.csproj: Created SMS object that can be passed as   JSON through POST on Intermediary controller.
    * appsettings.json:
    * appsettings.Development.json: Will need to get TwilioCredentials
      from Credentials, but for development using UserSecrets.

---
+++ EMAStudySMSWebAPI/Models/Scheduler/SMS.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EMAStudySMSWebAPI.Models.Scheduler
+{
+    public class SMS
+    {
+        public string recipientPhoneNumber { get; set; }
+        public string senderPhoneNumber { get; set; }
+        public string body { get; set; }
+    }
+}
